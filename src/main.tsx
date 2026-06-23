935e8d33728807f77c7a66aed6cc2e92030181b5
itspipedev <andresescobarbeltran@gmail.com>
2026-06-23 08:22:23 -0500

    🐛 DOM-0005: Silence React Router v7 deprecation warnings

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HeroUIProvider>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Navigate to="/planificar" replace />} />
